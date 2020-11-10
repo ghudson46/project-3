3bf9aa1802164349dcd6922641c5953383d4f554
Garrett Hudson <garretthudson87@yahoo.com>
2020-11-09 21:27:33 -0500

    added a little code to search for youtube videos on app and play

--- project-3/client/src/components/Video/Video.js
+++ project-3/client/src/components/Video/Video.js
@@ -1,15 +1,43 @@
-// Dependencies
-import React from "react";
-import ReactPlayer from 'react-player';
+import React from 'react'
+import ReactPlayer from 'react-player'
 
-function Video() {
-    return (
-          <div className="container">
-            <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
-          </div>
+class Video extends React.Component{
+
+    constructor(props){
+      super(props);
+  
+      this.state = {
+        inputValue : "",
+        url:""
+      }
+    }
+  
+    handleChange = (event)=>{
+      this.setState({inputValue : event.target.value})
+    }
+  
+    handleSubmit = (event) =>{
+      event.preventDefault();
+      this.setState({url: this.state.inputValue})
+    }
+  
+    render(){
+      return (
+        <div className="App">
+          <header className="App-header">
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                  <input onChange={this.handleChange} style={{margin:"20px"}} className="form-control" type="text" placeholder="Input the vidoe url" />
+                  <button style={{margin:"20px"}} className="btn btn-primary">PLAY VIDEO</button>
+                </form>
+              
+            </div>
+            <ReactPlayer url={this.state.url} controls={true} />
+          </header>
+        </div>
       );
-}
-
-
-
-export default Video;
+    }
+    
+  }
+  
+  export default Video;
